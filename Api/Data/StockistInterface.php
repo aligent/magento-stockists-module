db917fc1cd73a523a6ed49d50af2a17a2c8f85ba
Matthew O'Loughlin <matthew.oloughlin@aligent.com.au>
2020-03-20 15:00:09 +1030

    Add interface and concrete methods for stockist data

--- Api/Data/StockistInterface.php
+++ Api/Data/StockistInterface.php
@@ -4,5 +4,146 @@ namespace Aligent\Stockists\Api\Data;
 
 interface StockistInterface
 {
-    //TODO: Add concrete methods for build-in columns.
+    /**
+     * @return int
+     */
+    public function getStockistId(): int;
+
+    /**
+     * @param int $id
+     * @return $this
+     */
+    public function setStockistId(int $id): StockistInterface;
+
+    /**
+     * @return string
+     */
+    public function getIdentifier(): string;
+
+    /**
+     * @param string $identifier
+     * @return $this
+     */
+    public function setIdentifier(string $identifier): StockistInterface;
+
+    /**
+     * @return float
+     */
+    public function getLat(): float;
+
+    /**
+     * @param float $lat
+     * @return $this
+     */
+    public function setLat(float $lat): StockistInterface;
+
+    /**
+     * @return float
+     */
+    public function getLng(): float;
+
+    /**
+     * @param float $lng
+     * @return $this
+     */
+    public function setLng(float $lng): StockistInterface;
+
+    /**
+     * @return string
+     */
+    public function getName(): string;
+
+    /**
+     * @param string $name
+     * @return $this
+     */
+    public function setName(string $name): StockistInterface;
+
+    /**
+     * @return array
+     */
+    public function getStreet(): array;
+
+    /**
+     * @param array $street
+     * @return $this
+     */
+    public function setStreet(array $street): StockistInterface;
+
+    /**
+     * @return string
+     */
+    public function getCity(): string;
+
+    /**
+     * @param string $city
+     * @return $this
+     */
+    public function setCity(string $city): StockistInterface;
+
+    /**
+     * @return string
+     */
+    public function getPostcode(): string;
+
+    /**
+     * @param string $postcode
+     * @return $this
+     */
+    public function setPostcode(string $postcode): StockistInterface;
+
+    /**
+     * @return string
+     */
+    public function getRegion(): string;
+
+    /**
+     * @param string $region
+     * @return $this
+     */
+    public function setRegion(string $region): StockistInterface;
+
+    /**
+     * @return string
+     */
+    public function getCountry(): string;
+
+    /**
+     * @param string $countryCode
+     * @return $this
+     */
+    public function setCountry(string $countryCode): StockistInterface;
+
+    /**
+     * @return string
+     */
+    public function getPhone(): string;
+
+    /**
+     * @param string $phone
+     * @return $this
+     */
+    public function setPhone(string $phone): StockistInterface;
+
+    /**
+     * @return array
+     */
+    public function getStoreIds(): array;
+
+    /**
+     * @param array $storeIds
+     * @return $this
+     */
+    public function setStoreIds(array $storeIds): StockistInterface;
+
+    /**
+     * @return array
+     */
+    public function getHours(): array;
+
+    /**
+     * @param array $hours
+     * @return $this
+     */
+    public function setHours(array $hours): StockistInterface;
 }
--- Model/Stockist.php
+++ Model/Stockist.php
@@ -16,6 +16,7 @@ class Stockist extends AbstractModel implements StockistInterface, IdentityInter
     const CACHE_TAG = 'aligent_stockist';
     protected $_cacheTag = 'aligent_stockist';
     protected $_eventPrefix = 'aligent_stockist';
+    protected $_idFieldName = 'stockist_id';
 
     protected function _construct()
     {
@@ -25,5 +26,239 @@ class Stockist extends AbstractModel implements StockistInterface, IdentityInter
     public function getIdentities()
     {
         return [self::CACHE_TAG . '_' . $this->getId()];
+    }
+
+    /**
+     * @return int
+     */
+    public function getStockistId(): int
+    {
+        return $this->getId();
+    }
+
+    /**
+     * @param int $id
+     * @return $this
+     */
+    public function setStockistId(int $id): StockistInterface
+    {
+        $this->setData('stockist_id', $id);
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIdentifier(): string
+    {
+        return $this->getData('identifier');
+    }
+
+    /**
+     * @param string $identifier
+     * @return $this
+     */
+    public function setIdentifier(string $identifier): StockistInterface
+    {
+        $this->setData('identifier', $identifier);
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLat(): float
+    {
+        return $this->getData('lat');
+    }
+
+    /**
+     * @param float $lat
+     * @return $this
+     */
+    public function setLat(float $lat): StockistInterface
+    {
+        $this->setData('lat', $lat);
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLng(): float
+    {
+        return $this->getData('lng');
+    }
+
+    /**
+     * @param float $lng
+     * @return $this
+     */
+    public function setLng(float $lng): StockistInterface
+    {
+        $this->setData('lng', $lng);
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->getData('name');
+    }
+
+    /**
+     * @param string $name
+     * @return $this
+     */
+    public function setName(string $name): StockistInterface
+    {
+        $this->setData('name', $name);
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getStreet(): array
+    {
+        return $this->getData('street');
+    }
+
+    /**
+     * @param array $street
+     * @return $this
+     */
+    public function setStreet(array $street): StockistInterface
+    {
+        $this->setData('street', $street);
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity(): string
+    {
+        return $this->getData('city');
+    }
+
+    /**
+     * @param string $city
+     * @return $this
+     */
+    public function setCity(string $city): StockistInterface
+    {
+        $this->setData('city', $city);
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPostcode(): string
+    {
+        return $this->getData('postcode');
+    }
+
+    /**
+     * @param string $postcode
+     * @return $this
+     */
+    public function setPostcode(string $postcode): StockistInterface
+    {
+        $this->setData('postcode', $postcode);
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRegion(): string
+    {
+        return $this->getData('region');
+    }
+
+    /**
+     * @param string $region
+     * @return $this
+     */
+    public function setRegion(string $region): StockistInterface
+    {
+        $this->setData('region', $region);
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountry(): string
+    {
+        return $this->getData('country');
+    }
+
+    /**
+     * @param string $countryCode
+     * @return $this
+     */
+    public function setCountry(string $countryCode): StockistInterface
+    {
+        $this->setData('country', $countryCode);
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhone(): string
+    {
+        return $this->getData('phone');
+    }
+
+    /**
+     * @param string $phone
+     * @return $this
+     */
+    public function setPhone(string $phone): StockistInterface
+    {
+        $this->setData('phone', $phone);
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getStoreIds(): array
+    {
+        return $this->getData('store_ids');
+    }
+
+    /**
+     * @param array $storeIds
+     * @return $this
+     */
+    public function setStoreIds(array $storeIds): StockistInterface
+    {
+        $this->setData('store_ids', $storeIds);
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getHours(): array
+    {
+        return $this->getData('hours');
+    }
+
+    /**
+     * @param array $hours
+     * @return $this
+     */
+    public function setHours(array $hours): StockistInterface
+    {
+        $this->setData('hours', $hours);
+        return $this;
     }
 }
