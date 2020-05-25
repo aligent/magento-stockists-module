75fd86bdc58a4bcc2d00a6add1c2bdd9ead70fc0
Matthew O'Loughlin <matthew.oloughlin@aligent.com.au>
2020-05-25 16:26:33 +0930

    Add ability to search for an individual location by it's identifier

---
+++ Model/Resolver/Stockist.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Aligent\Stockists\Model\Resolver;
+
+use Aligent\Stockists\Api\StockistRepositoryInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\Resolver\ContextInterface;
+use Magento\Framework\GraphQl\Query\Resolver\Value;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+
+class Stockist implements ResolverInterface
+{
+    /**
+     * @var StockistRepositoryInterface
+     */
+    private $stockistRepository;
+
+    public function __construct(StockistRepositoryInterface $stockistRepository)
+    {
+        $this->stockistRepository = $stockistRepository;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    {
+        try {
+            $result = $this->stockistRepository->get($args['identifier']);
+            $locationData = $result->getData();
+            $locationData['model'] = $result;
+            return $locationData;
+        } catch (NoSuchEntityException $ex) {
+            return [];
+        }
+    }
+}
