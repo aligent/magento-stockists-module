0b46d3431046d872d71e2884298388a95ca20108
torbjorn_vanheeswijck <torbjorn.vanheeswijck@aligent.com.au>
2020-06-09 11:54:34 +0930

    SPW-39: Create beforeSave plugin with Magicento

---
+++ Plugin/Model/StockistRepositoryPlugin.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Aligent\Stockists\Plugin\Model;
+
+class StockistRepositoryPlugin
+{
+    public function beforeSave(\Aligent\Stockists\Api\StockistRepositoryInterface $subject, \Aligent\Stockists\Api\Data\StockistInterface $stockist)
+    {
+
+    }
+}
