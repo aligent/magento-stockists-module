c2287aeab6e701bccdb7ec35131e57767e2edb6f
Lachlan Turner <lachlan.turner@aligent.com.au>
2022-03-24 13:52:25 +1030

    Fix incorrect comment

--- Model/GoogleMapsAdapter.php
+++ Model/GoogleMapsAdapter.php
@@ -104,7 +104,7 @@ class GoogleMapsAdapter implements AdapterInterface
         $response = $this->serialiser->unserialize($httpClient->getBody());
 
         /**
-         * Google Maps API return 200 and state any errors that occur as part
+         * Google Maps API returns status and state any errors that occur as part
          * of the api response body...
          *
          * @see: https://developers.google.com/maps/documentation/geocoding/overview#StatusCodes
